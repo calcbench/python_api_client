27600bf6c5bd665c8876b1ca11b44754f7015ce4
Andrew Kittredge <andrew@calcbench.com>
2016-09-21 14:55:02 -0400

    fixing imports

--- calcbench/__init__.py
+++ calcbench/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.8'
+__version__ = '0.1.0'
 from .api_client import (normalized_data, 
                          normalized_dataframe, 
                          tickers, 
@@ -13,4 +13,6 @@ from .api_client import (normalized_data,
                          available_metrics, 
                          text_search,
                          filings,
+                         mapped_raw,
+                         point_in_time,
                          )
