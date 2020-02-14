7c0a26290ef0c38ce673364edfe33ce40a0e268b
Andrew Kittredge <andrew@calcbench.com>
2020-02-14 16:40:22 -0500

    simplifying listener

--- calcbench/listener.py
+++ calcbench/listener.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 try:
-    from azure.servicebus import ServiceBusClient
-    from azure.servicebus.control_client import (
-        ServiceBusService,
-        Message,
-        Topic,
-        Rule,
-        DEFAULT_RULE_NAME,
-    )
+    from azure.servicebus import SubscriptionClient
 except ImportError:
     "Will not be able to use the listener"
     pass
@@ -47,18 +40,12 @@ def handle_filings(
         >>>     subscription_name=subscription,
         >>>     filter_expression="FilingType = 'annualQuarterlyReport'",
         >>> )
-
-
     """
-    sb_client = ServiceBusClient.from_connection_string(connection_string)
-    subscription = sb_client.get_subscription(TOPIC, subscription_name)
-    _create_filter(
-        sb_client.mgmt_client,
-        subscription_name=subscription_name,
-        filter_expression=filter_expression,
+    sub_client = SubscriptionClient.from_connection_string(
+        conn_str=connection_string, name=subscription_name, topic=TOPIC
     )
 
-    for message in subscription.get_receiver():        
+    for message in sub_client.get_receiver():
         try:
             body_bytes = b"".join(message.body)
             filing = json.loads(body_bytes)
@@ -70,27 +57,6 @@ def handle_filings(
             try:
                 handler(filing)
             except Exception as e:
-                logger.exception(f"Exception Processing {filing}\n{e}")                
+                logger.exception(f"Exception Processing {filing}\n{e}")
             else:
                 message.complete()
-
-
-def _create_filter(bus_service, subscription_name, filter_expression):
-    existing_rules = bus_service.list_rules(
-        topic_name=TOPIC, subscription_name=subscription_name
-    )
-    for rule in existing_rules:
-        bus_service.delete_rule(
-            topic_name=TOPIC, subscription_name=subscription_name, rule_name=rule.name
-        )
-
-    rule = Rule()
-    rule.filter_type = "SqlFilter"
-    rule.filter_expression = filter_expression
-    bus_service.create_rule(
-        topic_name=TOPIC,
-        subscription_name=subscription_name,
-        rule_name="CustomFilter",
-        rule=rule,
-    )
-
