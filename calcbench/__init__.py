f72acaa0bb8147516fa1bf44051c11d9640e8df8
Andrew Kittredge <andrewlkittredge@gmail.com>
2021-10-13 12:21:38 -0400

    version bump

--- calcbench/__init__.py
+++ calcbench/__init__.py
@@ -8,7 +8,7 @@ logger = logging.getLogger()
 logging.getLogger('calcbench').setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler(stream=sys.stdout))
 """
-__version__ = "5.2.2"
+__version__ = "5.3.0"
 from .api_client import (
     CompanyIdentifierScheme,
     as_reported_raw,
