a7331f5bf095442300d91e8cde6354241525e7c7
Andrew Kittredge <andrewlkittredge@gmail.com>
2024-01-10 09:54:54 -0500

    version bump

--- calcbench/api_client.py
+++ calcbench/api_client.py
@@ -48,7 +48,7 @@ from requests import RequestException
 
 logger = logging.getLogger(__name__)
 
-__version__ = "11.1.0"
+__version__ = "12.0.0"
 
 USER_AGENT = f"cb_python_api_client {__version__}"
 
