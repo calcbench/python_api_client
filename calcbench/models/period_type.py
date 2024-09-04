9de6c789fa3aa734d3e75a000f1e93462eeb4077
Andrew Kittredge <andrewlkittredge@gmail.com>
2024-09-04 11:45:21 -0400

    document period types

--- calcbench/models/period_type.py
+++ calcbench/models/period_type.py
@@ -3,6 +3,18 @@ from enum import Enum
 
 class PeriodType(str, Enum):
     Annual = "annual"
+    """
+    Annual
+    """
     Quarterly = "quarterly"
+    """
+    Quarterly
+    """
     Combined = "combined"
+    """
+    Annual and quarter data
+    """
     TrailingTwelveMonths = "TTM"
+    """
+    Trailing twelve months
+    """
