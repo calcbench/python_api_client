632abcdf42b04bb5aaca0e38aabf78e264c2be42
Andrew Kittredge <andrewlkittredge@gmail.com>
2024-09-17 10:52:41 -0400

    non XBRL label field

--- calcbench/models/trace.py
+++ calcbench/models/trace.py
@@ -5,6 +5,15 @@ from pydantic import BaseModel, Extra
 
 class TraceData(BaseModel, extra=Extra.allow):
     local_name: Optional[str]
+    """
+    The XBRL tag
+    """
+
+    non_XBRL_label: Optional[str]
+    """
+    for points extracted from non-XBRL documents
+    """
+
     negative_weight: bool
     XBRL_fact_value: Optional[Union[str, float, int]]
     fact_id: Optional[int] = None
