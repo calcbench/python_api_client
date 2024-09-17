b9523620809a392d4cd90e04794388ff90eaef7f
Andrew Kittredge <andrewlkittredge@gmail.com>
2024-09-17 13:22:16 -0400

    doc strings

--- calcbench/models/trace.py
+++ calcbench/models/trace.py
@@ -1,20 +1,23 @@
 from typing import Optional, Union
 
-from pydantic import BaseModel, Extra
+from pydantic import BaseModel
 
 
-class TraceData(BaseModel, extra=Extra.allow):
-    local_name: Optional[str]
+class TraceData(BaseModel, extra="allow"):
+    local_name: Optional[str] = None
     """
-    The XBRL tag
+    The XBRL tag, null for non-XBRL facts
     """
 
-    non_XBRL_label: Optional[str]
+    non_XBRL_label: Optional[str] = None
     """
     for points extracted from non-XBRL documents
     """
 
     negative_weight: bool
-    XBRL_fact_value: Optional[Union[str, float, int]]
+    XBRL_fact_value: Optional[Union[str, float, int]] = None
     fact_id: Optional[int] = None
+    """
+    calcbench XBRL fact ID
+    """
     dimensions: Optional[str] = None
